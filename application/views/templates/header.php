b337c8ceec8497f09d5724fdb6ca5c4ec26cddaf
zhangbailng945 <1207549344@qq.com>
2015-12-06 16:19:48 +0800

    搭建bootstrap和codeigniter框架雏形。----order by long

--- application/views/templates/header.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-
-<html>
-<head>
-  <title><?php echo $title;?></title>
-  <link href="http://localhost/LoachBlog/communal/bootstrap/css/bootstrap.css" rel="stylesheet" media="screen">
-  <link href="http://localhost/LoachBlog/communal/bootstrap/css/bootstrap-datetimepicker.min.css" rel="stylesheet" media="screen">
- </head>
- <body>
