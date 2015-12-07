7dd931dc3aab7fa4df4b4556c647f03ebb9b4df3
long@amiam.com <long@amiam.com>
2015-12-07 17:54:21 +0800

    編寫登錄控制器--by long

---
+++ application/models/Musers.php
@@ -0,0 +1,14 @@
+<?php
+  class Muser extends  CI_Model
+  {
+      public function __construct()
+      {
+        $this->load->database();
+      }
+      
+      public function user_login($username,$userpassword)
+      {
+         
+      }
+      
+  }
