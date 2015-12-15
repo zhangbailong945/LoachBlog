a5a03ad6faffbf6664c3e8206dab38315e272d87
bailong zhang <bailong zhang>
2015-12-15 23:11:19 +0800

    后台加入easyui，后台模板。-- by long

--- application/views/administrator/login.php
+++ application/views/administrator/login.php
@@ -1,7 +1,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>深圳市城建管理工作平台</title>
+<title></title>
 <style type="text/css">
 <!--
 body {
@@ -21,15 +21,15 @@ body {
     <td bgcolor="#1075b1">&nbsp;</td>
   </tr>
   <tr>
-    <td height="608" background="http://localhost/LoachBlog/communal/Administrator/images/admin/login_03.gif"><table width="847" border="0" align="center" cellpadding="0" cellspacing="0">
+    <td height="608" background="http://localhost/LoachBlog/communal/Administrator/images/login/login_03.gif"><table width="847" border="0" align="center" cellpadding="0" cellspacing="0">
       <tr>
-        <td height="318" background="http://localhost/LoachBlog/communal/Administrator/images/admin/login_04.gif">&nbsp;</td>
+        <td height="318" background="http://localhost/LoachBlog/communal/Administrator/images/login/login_04.gif">&nbsp;</td>
       </tr>
       <tr>
         <td height="84"><table width="100%" border="0" cellspacing="0" cellpadding="0">
           <tr>
-            <td width="381" height="84" background="http://localhost/LoachBlog/communal/Administrator/images/admin/login_06.gif">&nbsp;</td>
-            <td width="162" valign="middle" background="http://localhost/LoachBlog/communal/Administrator/images/admin/login_07.gif"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+            <td width="381" height="84" background="http://localhost/LoachBlog/communal/Administrator/images/login/login_06.gif">&nbsp;</td>
+            <td width="162" valign="middle" background="http://localhost/LoachBlog/communal/Administrator/images/login/login_07.gif"><table width="100%" border="0" cellspacing="0" cellpadding="0">
               <tr>
                 <td width="44" height="24" valign="bottom"><div align="right"><span class="STYLE3">账号</span></div></td>
                 <td width="10" valign="bottom">&nbsp;</td>
@@ -44,28 +44,28 @@ body {
                 <td height="24" colspan="2" valign="bottom"><input type="password" name="textfield2" id="textfield2" style="width:100px; height:17px; background-color:#87adbf; border:solid 1px #153966; font-size:12px; color:#283439; "></td>
               </tr>
               <tr>
-                <td height="24" valign="bottom"><div align="right"><span class="STYLE3">验证码</span></div></td>
+                <td height="24" valign="bottom"><div align="right"><span class="STYLE3">验码</span></div></td>
                 <td width="10" valign="bottom">&nbsp;</td>
                 <td width="52" height="24" valign="bottom"><input type="text" name="textfield3" id="textfield3" style="width:50px; height:17px; background-color:#87adbf; border:solid 1px #153966; font-size:12px; color:#283439; "></td>
                 <td width="62" valign="bottom"><div align="left"><a href="javascript:void(0)"><img id="authcode" src="<?php echo site_url().'/administrator/imgauthcode/show';?>" onclick="refresh('/administrator/imgauthcode/show')" width="48px" height="18px" /></a></div></td>
               </tr>
               <tr></tr>
             </table></td>
-            <td width="26"><img src="http://localhost/LoachBlog/communal/Administrator/images/admin/login_08.gif" width="26" height="84"></td>
-            <td width="67" background="http://localhost/LoachBlog/communal/Administrator/images/admin/login_09.gif"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+            <td width="26"><img src="http://localhost/LoachBlog/communal/Administrator/images/login/login_08.gif" width="26" height="84"></td>
+            <td width="67" background="http://localhost/LoachBlog/communal/Administrator/images/login/login_09.gif"><table width="100%" border="0" cellspacing="0" cellpadding="0">
               <tr>
-                <td height="25"><div align="center"><img src="http://localhost/LoachBlog/communal/Administrator/images/admin/dl.gif" width="57" height="20"></div></td>
+                <td height="25"><div align="center"><img src="http://localhost/LoachBlog/communal/Administrator/images/login/dl.gif" width="57" height="20"></div></td>
               </tr>
               <tr>
-                <td height="25"><div align="center"><img src="http://localhost/LoachBlog/communal/Administrator/images/admin/cz.gif" width="57" height="20"></div></td>
+                <td height="25"><div align="center"><img src="http://localhost/LoachBlog/communal/Administrator/images/login/cz.gif" width="57" height="20"></div></td>
               </tr>
             </table></td>
-            <td width="211" background="http://localhost/LoachBlog/communal/Administrator/images/admin/login_10.gif">&nbsp;</td>
+            <td width="211" background="http://localhost/LoachBlog/communal/Administrator/images/login/login_10.gif">&nbsp;</td>
           </tr>
         </table></td>
       </tr>
       <tr>
-        <td height="206" background="http://localhost/LoachBlog/communal/Administrator/images/admin/login_11.gif">&nbsp;</td>
+        <td height="206" background="http://localhost/LoachBlog/communal/Administrator/images/login/login_11.gif">&nbsp;</td>
       </tr>
     </table></td>
   </tr>
